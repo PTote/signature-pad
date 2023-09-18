a8c04adeb869e847caa35a2833d1575b994c6653
kposadas <kposadas@bytesw.com>
2023-09-18 14:19:54 -0600

    fix: touch screen draw

--- src/app/components/signature-pad/signature-pad.component.ts
+++ src/app/components/signature-pad/signature-pad.component.ts
@@ -33,11 +33,28 @@ export class SignaturePadComponent implements OnInit {
     this.getScreenSize();
   }
 
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(event: TouchEvent) {
+    const {touches} = event
+    this.startPointDrawing(touches[0].clientX, touches[0].clientY)
+  }
+
+  @HostListener('touchmove', ['$event'])
+  onTouchMove(event: TouchEvent) {
+    const {touches} = event
+    this.drawingMoves(touches[0].clientX, touches[0].clientY);
+  }
+
+  @HostListener('touchend', ['$event'])
+  onTouchEnd(event: TouchEvent) {
+    // Manejar evento de finalización de toque
+  }
+
   getScreenSize(): void {
     this.screenWidth = window.innerWidth;
     this.screenHeight = window.innerHeight;
 
-    if(this.ctx?.canvas){
+    if (this.ctx?.canvas) {
       this.changeSizeCanvas();
     }
 
@@ -45,141 +62,149 @@ export class SignaturePadComponent implements OnInit {
 
   changeSizeCanvas() {
 
-    if(this.screenWidth > 280 && this.screenWidth < 480 ){
+    if (this.screenWidth > 280 && this.screenWidth < 480) {
       this.ctx.canvas.width = 250
       this.ctx.canvas.height = 100
     }
 
-    if(this.screenWidth > 480 && this.screenWidth < 600 ){
+    if (this.screenWidth > 480 && this.screenWidth < 600) {
       this.ctx.canvas.width = 300
       this.ctx.canvas.height = 150
     }
 
-    if(this.screenWidth > 600 && this.screenWidth < 900 ){
+    if (this.screenWidth > 600 && this.screenWidth < 900) {
       this.ctx.canvas.width = 500
       this.ctx.canvas.height = 300
     }
 
-    if(this.screenWidth > 900){
+    if (this.screenWidth > 900) {
       this.ctx.canvas.width = 700
       this.ctx.canvas.height = 500
     }
 
   }
 
-ngAfterViewInit() {
-  this.ctx = this.canvas.nativeElement.getContext('2d');
-  if (this.btnDownLoadJPG && this.btnDownLoadPNG) {
-    this.btnDownLoadJPG.nativeElement.disabled = true;
-    this.btnDownLoadPNG.nativeElement.disabled = true;
+  ngAfterViewInit() {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    if (this.btnDownLoadJPG && this.btnDownLoadPNG) {
+      this.btnDownLoadJPG.nativeElement.disabled = true;
+      this.btnDownLoadPNG.nativeElement.disabled = true;
+    }
+
+    this.changeSizeCanvas();
   }
 
-  this.changeSizeCanvas();
-}
-
-// Manejador para el evento "dragover"
-onDragOver(event: DragEvent) {
-  event.preventDefault();
-  event.stopPropagation();
-}
-
-// Manejador para el evento "drop"
-onDrop(event: DragEvent) {
-  event.preventDefault();
-  event.stopPropagation();
-
-  const files = event.dataTransfer?.files;
-
-  if (files && files.length > 0) {
-    const file = files[0]; // Supongamos que solo se permite una imagen a la vez
-    this.loadImageFromFile(file);
+  // Manejador para el evento "dragover"
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
   }
-}
 
-// Carga la imagen desde el archivo y la muestra en el canvas
-loadImageFromFile(file: File) {
-  const reader = new FileReader();
+  // Manejador para el evento "drop"
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
 
-  reader.onload = (e: any) => {
-    const image = new Image();
-    image.src = e.target.result;
+    const files = event.dataTransfer?.files;
 
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0); // Dibuja la imagen en el canvas
-      this.isCanvasEmpty();
+    if (files && files.length > 0) {
+      const file = files[0]; // Supongamos que solo se permite una imagen a la vez
+      this.loadImageFromFile(file);
+    }
+  }
+
+  // Carga la imagen desde el archivo y la muestra en el canvas
+  loadImageFromFile(file: File) {
+    const reader = new FileReader();
+
+    reader.onload = (e: any) => {
+      const image = new Image();
+      image.src = e.target.result;
+
+      image.onload = () => {
+        this.ctx.drawImage(image, 0, 0); // Dibuja la imagen en el canvas
+        this.isCanvasEmpty();
+      };
     };
-  };
 
-  reader.readAsDataURL(file);
-
-}
-
-
-onMouseDown(event: MouseEvent) {
-  this.drawing = true;
-  [this.lastX, this.lastY] = [event.clientX - this.canvas.nativeElement.offsetLeft, event.clientY - this.canvas.nativeElement.offsetTop];
-}
-
-onMouseMove(event: MouseEvent) {
-  if (!this.drawing) return;
-
-  this.ctx.strokeStyle = 'black';
-  this.ctx.lineWidth = 3;
-  this.ctx.lineCap = 'round';
-  this.ctx.lineJoin = 'round';
-
-  this.ctx.beginPath();
-  this.ctx.moveTo(this.lastX, this.lastY);
-  [this.lastX, this.lastY] = [event.clientX - this.canvas.nativeElement.offsetLeft, event.clientY - this.canvas.nativeElement.offsetTop];
-  this.ctx.lineTo(this.lastX, this.lastY);
-  this.ctx.stroke();
-  this.isCanvasEmpty()
-}
-
-onMouseUp() {
-  this.drawing = false;
-}
-
-onMouseLeave() {
-  this.drawing = false;
-}
-
-clearCanvas() {
-  this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-}
-
-saveSignatureJPG() {
-  const signatureImage = this.canvas.nativeElement.toDataURL('image/jpg');
-  const a = document.createElement('a');
-  a.href = signatureImage;
-  a.download = 'test.jpg'
-  console.log(a.download);
-  a.click();
-}
-
-saveSignaturePNG() {
-  const signatureImage = this.canvas.nativeElement.toDataURL('image/png');
-  const a = document.createElement('a');
-  a.href = signatureImage;
-  a.download = 'test.png'
-  a.click();
-}
-
-isCanvasEmpty() {
-  const imageData = this.ctx.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-
-
-  const empty = imageData.data.some(value => value !== 0)
-
-  if (empty) {
-    this.btnDownLoadJPG.nativeElement.disabled = false;
-    this.btnDownLoadPNG.nativeElement.disabled = false;
+    reader.readAsDataURL(file);
 
   }
-}
 
-emitBase64IMG(): void {
-  return this.canvas.nativeElement.toDataURL('image/png');
-}
+
+  private startPointDrawing(clientX: number, clientY: number): void{
+    this.drawing = true;
+    [this.lastX, this.lastY] = [clientX - this.canvas.nativeElement.offsetLeft, clientY - this.canvas.nativeElement.offsetTop];
+  }
+
+  private drawingMoves(clientX: number, clientY: number): void{
+    if (!this.drawing) return;
+
+    this.ctx.strokeStyle = 'black';
+    this.ctx.lineWidth = 3;
+    this.ctx.lineCap = 'round';
+    this.ctx.lineJoin = 'round';
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.lastX, this.lastY);
+    [this.lastX, this.lastY] = [clientX - this.canvas.nativeElement.offsetLeft, clientY - this.canvas.nativeElement.offsetTop];
+    this.ctx.lineTo(this.lastX, this.lastY);
+    this.ctx.stroke();
+    this.isCanvasEmpty()
+  }
+
+  onMouseDown(event: MouseEvent): void {
+    this.startPointDrawing(event.clientX, event.clientY);
+  }
+
+  onMouseMove(event: MouseEvent): void {
+    this.drawingMoves(event.clientX, event.clientY);
+  }
+
+  onMouseUp() {
+    this.drawing = false;
+  }
+
+  onMouseLeave() {
+    this.drawing = false;
+  }
+
+  clearCanvas() {
+    this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+  }
+
+  saveSignatureJPG() {
+    const signatureImage = this.canvas.nativeElement.toDataURL('image/jpg');
+    const a = document.createElement('a');
+    a.href = signatureImage;
+    a.download = 'test.jpg'
+    console.log(a.download);
+    a.click();
+  }
+
+  saveSignaturePNG() {
+    const signatureImage = this.canvas.nativeElement.toDataURL('image/png');
+    const a = document.createElement('a');
+    a.href = signatureImage;
+    a.download = 'test.png'
+    a.click();
+  }
+
+  isCanvasEmpty() {
+    const imageData = this.ctx.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+
+
+    const empty = imageData.data.some(value => value !== 0)
+
+    if (empty) {
+      this.btnDownLoadJPG.nativeElement.disabled = false;
+      this.btnDownLoadPNG.nativeElement.disabled = false;
+
+    }
+  }
+
+  emitBase64IMG(): void {
+    return this.canvas.nativeElement.toDataURL('image/png');
+  }
 
 }
